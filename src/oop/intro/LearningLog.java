f8540ac0c0b4e8d603af5cb2b7d159a9a9512868
marcel615 <alswo000615@gmail.com>
2026-05-07 17:36:39 +0900

    refactor: oop.intro 내 클래스 기본 객체 연습 코드로 변경

--- src/oop/intro/LearningLog.java
+++ src/oop/intro/LearningLog.java
@@ -1,38 +1,19 @@
 package oop.intro;
 
 public class LearningLog {
-    //필드
-    private String title;
-    private int minutes;
-    private boolean publicFlag;
 
-    //ctor
-    public LearningLog(String title, int minutes, boolean publicFlag){
-        this.title = title;
-        this.minutes = minutes;
-        this.publicFlag = publicFlag;
-    }
+    // 객체의 속성을 필드(field)
+    String title;
+    int minutes;
+    boolean publicLog;
 
-    //getter, setter
-    public String getTitle(){
-        return title;
-    }
-    public int getMinutes(){
-        return minutes;
-    }
-    public boolean getPublicFlag(){
-        return publicFlag;
-    }
-
-    //method
-    public void printSummary(){
+    void printSummary() {
         System.out.println(title + " - " + minutes + "분");
     }
-    public boolean isNeedReview(){
-        return (minutes < 60);
+
+    boolean needsReview() {
+        return minutes < 60;
     }
-
-
 
 
 }
--- src/oop/intro/SprintLogApp.java
+++ src/oop/intro/SprintLogApp.java
@@ -2,24 +2,49 @@ package oop.intro;
 
 public class SprintLogApp {
     public static void main(String[] args) {
-        // 객체들 생성
-        LearningLog javaLog1 = new LearningLog("Java 시작", 40, true);
-        LearningLog javaLog2 = new LearningLog("Git 복습", 30, false);
-        LearningLog javaLog3 = new LearningLog("조건문 학습", 75, true);
-        LearningLog javaLog4 = new LearningLog("반복문 학습", 120, true);
 
-        LearningLog[] javaLogs = new LearningLog[]{javaLog1, javaLog2, javaLog3, javaLog4};
+        LearningLog javaLog = new LearningLog();
+        javaLog.title = "Java 시작";
+        javaLog.minutes = 40;
+        javaLog.publicLog = true;
 
-        for (LearningLog log : javaLogs){
-            if (log.isNeedReview())
-                log.printSummary();
+        LearningLog gitLog = new LearningLog();
+        gitLog.title = "Git 복습";
+        gitLog.minutes = 30;
+        gitLog.publicLog = false;
+
+        LearningLog conditionLog = new LearningLog();
+        conditionLog.title = "조건문 학습";
+        conditionLog.minutes = 75;
+        conditionLog.publicLog = true;
+
+        LearningLog loopLog = new LearningLog();
+        loopLog.title = "반복문 학습";
+        loopLog.minutes = 120;
+        loopLog.publicLog = true;
+
+        LearningLog[] logs = {javaLog, gitLog, conditionLog, loopLog};
+
+        System.out.println("==== 객체로 관리하는 학습 기록 ===");
+        for (int i = 0; i < logs.length; i++) {
+            logs[i].printSummary();
         }
 
-        System.out.println("=================");
-        for (LearningLog log : javaLogs){
-            if (log.getPublicFlag())
+        System.out.println();
+        System.out.println("==== 복습 필요 기록 ====");
+        for (LearningLog log : logs) {
+            if (log.needsReview()) {
                 log.printSummary();
+            }
         }
 
+        System.out.println();
+        System.out.println("==== 공개 기록 ====");
+        for (LearningLog log : logs) {
+            if (!log.publicLog) {
+                continue;
+            }
+            log.printSummary();
+        }
     }
 }
