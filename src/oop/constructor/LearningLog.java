994c37a129628cdf9510c14d892a30138da494e4
marcel615 <alswo000615@gmail.com>
2026-05-07 18:23:24 +0900

    feat: 생성자 기반 객체화 연습
    - LearningLog 클래스에 생성자 추가
    - main에서 객체를 다양한 생성자를 통해 생성
    - this 키워드를 활용한 여러가지 문법 학습

---
+++ src/oop/constructor/LearningLog.java
@@ -0,0 +1,44 @@
+package oop.constructor;
+
+public class LearningLog {
+    //필드
+    private String title;
+    private int minutes;
+    private boolean publicFlag;
+
+    //ctor
+    public LearningLog(){
+
+    }
+    public LearningLog(String title, int minutes){
+        this(title, minutes, true);
+    }
+    public LearningLog(String title, int minutes, boolean publicFlag){
+        this.title = title;
+        this.minutes = minutes;
+        this.publicFlag = publicFlag;
+    }
+
+    //getter, setter
+    public String getTitle(){
+        return title;
+    }
+    public int getMinutes(){
+        return minutes;
+    }
+    public boolean getPublicFlag(){
+        return publicFlag;
+    }
+
+    //method
+    public void printSummary(){
+        System.out.println(title + " - " + minutes + "분");
+    }
+    public boolean isNeedReview(){
+        return (minutes < 60);
+    }
+
+
+
+
+}
