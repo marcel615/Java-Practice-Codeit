bac6e0340807d04d10efcfcaf87a70e4539c7e23
marcel615 <alswo000615@gmail.com>
2026-05-08 13:04:19 +0900

    feat: static 보강
    - Calculator 예시 추가

--- src/oop/static_final/Calculator.java
+++ src/oop/static_final/Calculator.java
@@ -5,7 +5,10 @@ public class Calculator {
     //필드
     private String color;
     static double pi = 3.141592d;
-    
+
+    public static double cal(double radius){
+        return radius * radius * pi;
+    }
 
 
 
